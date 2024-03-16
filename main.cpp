229b453dd3127581b238548211037bc6488303aa
YeilerMR <yeiler.montes.rojas@est.una.ac.cr>
2024-03-16 11:27:09 -0600

    Version Yeiler prueba Proyecto

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: yeile
+ *
+ * Created on 16 de marzo de 2024, 11:20
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("Hola mundo XD\n");
+    return 0;
+}
+
